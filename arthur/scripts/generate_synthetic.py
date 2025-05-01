b4225127bc8f1712b358d08f0e476b4a4f7c79de
Arthur De Los Santos <arthurdls04@gmail.com>
2025-04-30 22:29:01 -0400

    add data saving for synthetic 3D scene

--- arthur/scripts/generate_synthetic.py
+++ arthur/scripts/generate_synthetic.py
@@ -1,6 +1,7 @@
 import numpy as np
 import open3d as o3d
 import cv2
+from utils import save_dataset_h5
 
 def make_scene():
     boxes = []
@@ -19,6 +20,7 @@ def make_scene():
 def render_frames(scene, n_frames=100):
     width=640 
     height=480
+    scene_folder = 'data/raw/scene1/'
 
     vis = o3d.visualization.Visualizer()
     vis.create_window(width=width, height=height, visible=False)
@@ -27,8 +29,13 @@ def render_frames(scene, n_frames=100):
 
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     fps = 30
-    video = cv2.VideoWriter('data/raw/test.mp4', fourcc, float(fps), (width, height))
+    video = cv2.VideoWriter(scene_folder + 'video.mp4', fourcc, float(fps), (width, height))
 
+    rgb_list = []
+    depth_list = []
+    pos_list = []
+    intrinsics = []
+    extrinsics = []
     for i in range(n_frames):
         theta = 2*np.pi * i/n_frames
         cam_pos = [2*np.cos(theta), 0.5, 2*np.sin(theta)]
@@ -36,15 +43,27 @@ def render_frames(scene, n_frames=100):
         ctr.set_up([0,1,0])
         ctr.set_front((np.array([0,0,0]) - cam_pos) / np.linalg.norm(cam_pos))
         ctr.set_zoom(0.5)
+        param = ctr.convert_to_pinhole_camera_parameters()
+        intrinsic = param.intrinsic
+        extrinsic = param.extrinsic
         vis.poll_events(); vis.update_renderer()
-        rgb = np.asarray(vis.capture_screen_float_buffer()) * 255
-        rgb = rgb.astype(np.uint8)
+        rgb = np.asarray(vis.capture_screen_float_buffer()) 
+        rgb_scaled = (rgb * 255).astype(np.uint8)
         depth = np.asarray(vis.capture_depth_float_buffer())
+        rgb_list.append(rgb)
+        depth_list.append(depth)
+        pos_list.append(cam_pos)
+        intrinsics.append(intrinsic.intrinsic_matrix)
+        extrinsics.append(extrinsic)
 
         # save rgb and depth
-        video.write(rgb)
+        video.write(rgb_scaled)
     vis.destroy_window()
     video.release()
+    rgb_list = np.array(rgb_list)
+    depth_list = np.array(depth_list)
+
+    save_dataset_h5(scene_folder + 'dataset.h5', rgb_list, depth_list, pos_list, intrinsics, extrinsics)
 
 if __name__ == "__main__":
     scene = make_scene()
